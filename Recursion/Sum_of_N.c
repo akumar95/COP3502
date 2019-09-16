2486a2a75928a576eb1cb7dc7d6ba6c1a8c9972b
Aneesh Kumar <akumar4@knights.ucf.edu>
2019-09-15 23:11:48 -0400

    Inital Commit

---
+++ Recursion/Sum_of_N.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// This program sums the value of n recursively (n-1);
+
+int sum(int n)
+{
+  if (n == 0)
+    return 0;
+  return sum(n - 1) + n;
+}
+
+// For-loop version (interative).
+int iSum(int n)
+{
+  int s = 0;
+  for (int i = 0; i <= n; i++)
+    s += i;
+  return s;
+}
+
+int main()
+{
+
+  // Assign a temp value of sum function then print.
+  int r;
+  r = sum(5);
+  printf("%d\n", r);
+
+  return 0;
+}
